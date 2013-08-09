9d698115656b9c19b6ac17e2f802ef4f7169219e
Peter Kerepesi <peter@low.hu>
2013-08-09 00:08:41 -0700

    [CT] Skeleton controllers added

---
+++ controllers/controller.site.php
@@ -0,0 +1,106 @@
+<?php
+
+/**
+ * MyProjectNameHere <http://www.example.com>
+ * Controller Class
+ *
+ * It is recommended to extend Controller classes from WWW_Factory in order to
+ * provide various useful functions and API access for the Controller.
+ *
+ * @package    Factory
+ * @author     DeveloperNameHere <email@example.com>
+ * @copyright  Copyright (c) 2012, ProjectOwnerNameHere
+ * @license    Unrestricted
+ * @tutorial   /doc/pages/guide_mvc.htm
+ * @since      1.0.0
+ * @version    1.0.0
+ */
+
+class WWW_controller_site extends WWW_Factory {
+
+    /**
+     * Simple example call to get data
+     *
+     */
+    public function crud_create($configuration){
+    }
+
+    /**
+     * Simple example call to get data
+     *
+     */
+    public function crud_read($configuration){
+    }
+
+    /**
+     * Simple example call to get data
+     *
+     */
+    public function crud_update($configuration){
+    }
+
+    /**
+     * Simple example call to get data
+     *
+     */
+    public function crud_delete($configuration){
+    }
+
+    /**
+     * Simple example call to get data
+     *
+     */
+    public function biz_login($configuration){
+    }
+
+    /**
+     * Simple example call to get data
+     *
+     */
+    public function biz_join_news($configuration){
+    }
+
+    /**
+     * Simple example call to get data
+     *
+     */
+    public function biz_remove_news($configuration){
+    }
+
+    /**
+     * Simple example call to get data
+     *
+     */
+    public function biz_forgot_password($configuration){
+    }
+
+    /**
+     * Simple example call to get data
+     *
+     */
+    public function biz_activate_registration($configuration){
+    }
+
+    /**
+     * Simple example call to get data
+     *
+     */
+    public function biz_post_social($configuration){
+    }
+
+    /**
+     * Simple example call to get data
+     *
+     */
+    public function biz_invite_user($configuration){
+    }
+
+    /**
+     * Simple example call to get data
+     *
+     */
+    public function biz_pay($configuration){
+    }
+}
+
+?>
---
+++ controllers/controller.user.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * MyProjectNameHere <http://www.example.com>
+ * Controller Class
+ *
+ * It is recommended to extend Controller classes from WWW_Factory in order to
+ * provide various useful functions and API access for the Controller.
+ *
+ * @package    Factory
+ * @author     DeveloperNameHere <email@example.com>
+ * @copyright  Copyright (c) 2012, ProjectOwnerNameHere
+ * @license    Unrestricted
+ * @tutorial   /doc/pages/guide_mvc.htm
+ * @since      1.0.0
+ * @version    1.0.0
+ */
+
+class WWW_controller_user extends WWW_Factory {
+
+    /**
+     * Simple example call to get data
+     *
+     */
+    public function crud_create($configuration){
+    }
+
+    /**
+     * Simple example call to get data
+     *
+     */
+    public function crud_read($configuration){
+    }
+
+    /**
+     * Simple example call to get data
+     *
+     */
+    public function crud_update($configuration){
+    }
+
+    /**
+     * Simple example call to get data
+     *
+     */
+    public function crud_delete($configuration){
+    }
+
+    /**
+     * Simple example call to get data
+     *
+     */
+    public function biz_gopro($configuration){
+    }
+
+    /**
+     * Simple example call to get data
+     *
+     */
+    public function biz_trust($configuration){
+    }
+}
+
+?>
